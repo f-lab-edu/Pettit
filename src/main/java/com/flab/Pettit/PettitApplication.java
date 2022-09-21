fbd3a838e615c55d9a20625db3185d937ab2151c
kidae92 <kidae92@hanmail.net>
2022-09-21 14:24:54 +0900

    initialize project

---
+++ src/main/java/com/flab/Pettit/PettitApplication.java
@@ -0,0 +1,13 @@
+package com.flab.Pettit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettitApplication.class, args);
+	}
+
+}
