0d60295f2a50fa6429dbb825bc826a544be6c263
kidae92 <kidae92@hanmail.net>
2022-10-14 10:41:19 +0900

    init_project

---
+++ src/test/java/com/flab/Pettit/PettitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.Pettit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
