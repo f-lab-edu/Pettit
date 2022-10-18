1eaed7aa98a61f45b4736ae5d9c6f08c3dcc9ffb
kidae92 <kidae92@hanmail.net>
2022-10-18 20:31:50 +0900

    controller에서 Post형식으로 요청이오고 BoardService의 savePost를 시행하게 됨. savePost는 Repository를 통해 실제로 데이터를 저장하게 된다. repository의 save는 JpaRepository -> PagingAndSortingReposity -> CrudRepository의 인퍼페이스

--- src/main/java/com/flab/Pettit/service/BoardService.java
+++ src/main/java/com/flab/Pettit/service/BoardService.java
@@ -1,2 +1,23 @@
-package com.flab.Pettit.service;public class BoardService {
+package com.flab.Pettit.service;
+
+import com.flab.Pettit.dto.BoardDto;
+import com.flab.Pettit.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+    /**
+     savePost는 Ropository를 통해 실제로 데이터를 저장하게 된다.
+     repository의 save는 JpaRepository -> PagingAndSortingRepository -> CrudRepository의 인터페이스이다
+     **/
+    @Transactional
+    public Long savePost(BoardDto boardDto){
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
 }
