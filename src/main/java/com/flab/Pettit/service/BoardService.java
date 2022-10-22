136887cc92b4cf921904628553b416fe67439cc1
kidae92 <kidae92@hanmail.net>
2022-10-22 15:24:41 +0900

    게시글 조회 서비스 기능 구현

--- src/main/java/com/flab/Pettit/service/BoardService.java
+++ src/main/java/com/flab/Pettit/service/BoardService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoardService {
@@ -29,7 +30,7 @@ public class BoardService {
     repository에서 모든 데이터를 가져와, 데이터만큼 반복하면서 BoardDto 타입의 List에 데이터를 파싱하여 집어넣고
     완성된 BoardDto 타입의 List를 리턴해준다.
     **/
-    @Transactional
+    @Transactional(readOnly = true)
     public List<BoardDto> findAll(){
         List<BoardEntity> boards = boardRepository.findAll();
         List<BoardDto> boardDtoList = new ArrayList<>();
@@ -45,5 +46,21 @@ public class BoardService {
             boardDtoList.add(boardDto);
         }
         return boardDtoList;
+    }
+    /**
+    게시글 - 상세 조회
+    **/
+    @Transactional(readOnly = true)
+    public BoardDto findById(Long id) {
+        Optional<BoardEntity> boardWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardWrapper.get();
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createDate(boardEntity.getCreatDate())
+                .build();
+        return boardDto;
     }
 }
