c6d37db75ad97ffc5b0bb3f05c5b87c720ca87d4
kidae92 <kidae92@hanmail.net>
2022-10-24 15:44:58 +0900

    특정 게시물 삭제 controller 작성

--- src/main/java/com/flab/Pettit/controller/BoardController.java
+++ src/main/java/com/flab/Pettit/controller/BoardController.java
@@ -20,25 +20,30 @@ import java.util.List;
 public class BoardController {
     private final BoardService boardService;
  // 모든 게시물 조회
-    @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<BoardDto>> findAll() {
         List<BoardDto> boardDtoList = boardService.findAll();
         return new ResponseEntity<List<BoardDto>>(boardDtoList, HttpStatus.OK);
     }
-
+ // 특정 게시물 조회
     @GetMapping(value = "/post/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<BoardDto> findById(@PathVariable("id") Long id){
         BoardDto boardDto = boardService.findById(id);
         return new ResponseEntity<BoardDto>(boardDto, HttpStatus.OK);
     }
-
-
  // 게시글 작성
     @PostMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Long> write(@RequestBody BoardDto boardDto) throws Exception {
         Long savedBoardSeq = boardService.savePost(boardDto);
         return new ResponseEntity<Long>(savedBoardSeq, HttpStatus.CREATED);
     }
+ // 특정 게시글 삭제
+    @DeleteMapping(value = "/post/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Long> delete(@PathVariable("id") Long id){
+        boardService.deletePost(id);
+        return new ResponseEntity<Long>(id, HttpStatus.NO_CONTENT);
+    }
+ // 특정 게시글 수정
 
 
 
