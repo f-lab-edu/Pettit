e11f5463740eef1bfa2a991bce487c526c914c76
kidae92 <kidae92@hanmail.net>
2022-10-24 16:32:55 +0900

    DTO 분리에 따른 코드 수정

--- src/main/java/com/flab/Pettit/controller/BoardController.java
+++ src/main/java/com/flab/Pettit/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.flab.Pettit.controller;
 
-import com.flab.Pettit.dto.BoardDto;
+import com.flab.Pettit.dto.BoardResponseDto;
+import com.flab.Pettit.dto.BoardSaveRequestDto;
 import com.flab.Pettit.service.BoardService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -21,19 +22,19 @@ public class BoardController {
     private final BoardService boardService;
  // 모든 게시물 조회
     @GetMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<List<BoardDto>> findAll() {
-        List<BoardDto> boardDtoList = boardService.findAll();
-        return new ResponseEntity<List<BoardDto>>(boardDtoList, HttpStatus.OK);
+    public ResponseEntity<List<BoardResponseDto>> findAll() {
+        List<BoardResponseDto> boardDtoList = boardService.findAll();
+        return new ResponseEntity<List<BoardResponseDto>>(boardDtoList, HttpStatus.OK);
     }
  // 특정 게시물 조회
     @GetMapping(value = "/post/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<BoardDto> findById(@PathVariable("id") Long id){
-        BoardDto boardDto = boardService.findById(id);
-        return new ResponseEntity<BoardDto>(boardDto, HttpStatus.OK);
+    public ResponseEntity<BoardResponseDto> findById(@PathVariable("id") Long id){
+        BoardResponseDto boardResponseDto = boardService.findById(id);
+        return new ResponseEntity<BoardResponseDto>(boardResponseDto, HttpStatus.OK);
     }
  // 게시글 작성
     @PostMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Long> write(@RequestBody BoardDto boardDto) throws Exception {
+    public ResponseEntity<Long> write(@RequestBody BoardSaveRequestDto boardDto) throws Exception {
         Long savedBoardSeq = boardService.savePost(boardDto);
         return new ResponseEntity<Long>(savedBoardSeq, HttpStatus.CREATED);
     }
