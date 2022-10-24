d36a1a6361b56c8656b033e5782f2f81ee7d15ae
kidae92 <kidae92@hanmail.net>
2022-10-24 15:44:12 +0900

    모든 게시물 조회 test code작성, 특정 게시글 삭제 test code 작성

--- src/test/java/com/flab/Pettit/controller/BoardControllerTest.java
+++ src/test/java/com/flab/Pettit/controller/BoardControllerTest.java
@@ -9,13 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -29,6 +25,26 @@ class BoardControllerTest {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    /**
+     모든 게시물 조회 테스트 코드
+     Status 일치 여부와 Body 길이가 0L보다 큰지 확인
+     모든 게시물을 boardRepository에서 가져와 List로 담은 다음 각각 isEqualTo로 테스트시 넣어준 값과 일치하는지를 확인한다.
+     **/
+    @Test
+    public void findAll() throws Exception {
+        String url = "http://localhost:" + port + "/api/post";
+
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity(url, String.class);
+        System.out.println("responseEntity.getBody() = " + responseEntity.getBody());
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains("id");
+        assertThat(responseEntity.getBody()).contains("title");
+        assertThat(responseEntity.getBody()).contains("writer");
+        assertThat(responseEntity.getBody()).contains("content");
+
+    }
 
     /**
     게시물 저장에 관한 테스트 코드
@@ -77,6 +93,33 @@ class BoardControllerTest {
         assertThat(responseEntity.getBody()).contains("content");
         assertThat(responseEntity.getBody()).contains("writer");
     }
+    /**
+    특정 게시물 삭제
+    **/
+    @Test
+    public void delete() throws Exception{
+        String title = "삭제테스트";
+        String writer = "홍기대";
+        String content = "삭제내용";
+
+        BoardEntity saveBoard = boardRepository.save(BoardEntity.builder()
+                .title(title)
+                .content(content)
+                .writer(writer)
+                .build());
+
+        Long boardId = saveBoard.getId();
+
+        String url = "http://localhost:" + port + "/api/post/" + boardId;
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<Object> httpEntity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<Long> responseEntity = testRestTemplate.exchange(url, HttpMethod.DELETE, httpEntity, Long.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+    }
+
 
 
 }
