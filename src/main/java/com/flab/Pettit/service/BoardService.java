e233d08d093c127e1871e6bb76b32ead54fb15c8
kidae92 <kidae92@hanmail.net>
2022-10-26 14:05:05 +0900

    특정 id를 찾아서 게시물 업데이트하는 로직 작성

--- src/main/java/com/flab/Pettit/service/BoardService.java
+++ src/main/java/com/flab/Pettit/service/BoardService.java
@@ -3,6 +3,7 @@ package com.flab.Pettit.service;
 import com.flab.Pettit.domain.entity.BoardEntity;
 import com.flab.Pettit.dto.BoardResponseDto;
 import com.flab.Pettit.dto.BoardSaveRequestDto;
+import com.flab.Pettit.dto.BoardUpdateRequestDto;
 import com.flab.Pettit.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,12 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class BoardService {
-
+    /**
+    final키워드를 사용한 인스턴스들만 Bean객체로 등록하게됨
+    final 키워두를 사용하지 않으면 테스트 코드에서 NullPointerException이 발생함
+    **/
     private final BoardRepository boardRepository;
+
     /**
     savePost는 Ropository를 통해 실제로 데이터를 저장하게 된다.
     repository의 save는 JpaRepository -> PagingAndSortingRepository -> CrudRepository의 인터페이스이다
@@ -51,6 +56,15 @@ public class BoardService {
         BoardEntity boardEntity = boardRepository.findById(id)
                 .orElseThrow(() -> new IllegalAccessError("[id=" + id + "] 해당 게시글이 존재하지 않습니다."));
         boardRepository.delete(boardEntity);
-
+    }
+    /**
+    게시글 - 수정 기능
+    **/
+    @Transactional
+    public Long update(Long id, BoardUpdateRequestDto boardUpdateRequestDto) {
+        BoardEntity boardEntity = boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalAccessError("[boardSeq=" + id + "] 해당 게시글이 존재하지 않습니다."));
+        boardEntity.update(boardUpdateRequestDto.getTitle(), boardUpdateRequestDto.getContent());
+        return id;
     }
 }
