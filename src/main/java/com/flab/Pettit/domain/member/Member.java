5768edc1b614452c5bd08ef6174e45f7596076c1
kidae92 <kidae92@hanmail.net>
2022-11-03 19:28:43 +0900

    member entity 생성

--- src/main/java/com/flab/Pettit/domain/member/Member.java
+++ src/main/java/com/flab/Pettit/domain/member/Member.java
@@ -1,2 +1,59 @@
-package com.flab.Pettit.domain.member;public class Member {
+package com.flab.Pettit.domain.member;
+
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.flab.Pettit.domain.entity.TimeEntity;
+import javax.persistence.*;
+
+@Table(name = "MEMBER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@AllArgsConstructor
+@Builder
+public class Member extends TimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String nickName;
+
+    @Column(nullable = false, length = 30)
+    private Integer age;
+
+
+
+  //정보 수정
+    public void updatePassword(PasswordEncoder passwordEncoder, String password){
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void updateName(String name){
+        this.name = name;
+    }
+
+    public void updateNickName(String nickName){
+        this.nickName = nickName;
+    }
+
+    public void updateAge(int age){
+        this.age = age;
+    }
+
+  //패스워드 암호화
+    public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
+
 }
