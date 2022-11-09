1e15c35cfb583e94b1762d8af07acfd1e64d39b7
kidae92 <kidae92@hanmail.net>
2022-11-09 20:16:42 +0900

    주석 위치 수정

--- src/main/java/com/flab/Pettit/config/SecurityConfig.java
+++ src/main/java/com/flab/Pettit/config/SecurityConfig.java
@@ -13,15 +13,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+    json을 통해 로그인을 진행, 로그인 이후 refreshToken이 만료되기 전까지는 토큰을 통해 인증을 진행할 것이기 때문에
+    formLogin, httpBasic, csrf 을 모두 disable 처리하였고, 세션은 Stateless(무상태)로 설정
 
+    로그인(/login), 회원가입(/signUp), 메인페이지(/)에 대해서는 인증 없이도 접근 가능
+    **/
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /**
-         json을 통해 로그인을 진행, 로그인 이후 refreshToken이 만료되기 전까지는 토큰을 통해 인증을 진행할 것이기 때문에
-         formLogin, httpBasic, csrf 을 모두 disable 처리하였고, 세션은 Stateless(무상태)로 설정
 
-         로그인(/login), 회원가입(/signUp), 메인페이지(/)에 대해서는 인증 없이도 접근 가능
-         **/
         http
                 .formLogin().disable()//1 - formLogin 인증방법 비활성화
                 .httpBasic().disable()//2 - httpBasic 인증방법 비활성화(특정 리소스에 접근할 때 username과 password 물어봄)
