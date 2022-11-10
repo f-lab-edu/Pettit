d87f01b16a1c5ffc54faba8915ec886dd3d83fa3
kidae92 <kidae92@hanmail.net>
2022-11-10 18:37:13 +0900

    주석처리 빈칸 삭제

--- src/main/java/com/flab/Pettit/config/SecurityConfig.java
+++ src/main/java/com/flab/Pettit/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- *
  * json을 통해 로그인을 진행, 로그인 이후 refreshToken이 만료되기 전까지는 토큰을 통해 인증을 진행할 것이기 때문에
  * formLogin, httpBasic, csrf 을 모두 disable 처리하였고, 세션은 Stateless(무상태)로 설정
  * 로그인(/login), 회원가입(/signUp), 메인페이지(/)에 대해서는 인증 없이도 접근 가능하게 설정
