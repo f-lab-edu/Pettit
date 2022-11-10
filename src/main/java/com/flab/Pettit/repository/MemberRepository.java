86eca3c41d60df369d479d6475d282bacdf6d087
kidae92 <kidae92@hanmail.net>
2022-11-10 13:17:19 +0900

    memberRepository생성 및 테스트 코드 작성

--- src/main/java/com/flab/Pettit/repository/MemberRepository.java
+++ src/main/java/com/flab/Pettit/repository/MemberRepository.java
@@ -1,2 +1,12 @@
-package com.flab.Pettit.repository;public interface MemberRepository {
+package com.flab.Pettit.repository;
+
+import com.flab.Pettit.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
