cdd1fb436243e5581d90a409f77b63b0e21878c0
kidae92 <kidae92@hanmail.net>
2022-11-11 12:39:36 +0900

    LoginService 코드 작성

--- src/main/java/com/flab/Pettit/service/LoginService.java
+++ src/main/java/com/flab/Pettit/service/LoginService.java
@@ -1,2 +1,26 @@
-package com.flab.Pettit.service;public class LoginService {
+package com.flab.Pettit.service;
+
+import com.flab.Pettit.domain.member.Member;
+import com.flab.Pettit.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("아이디가 없습니다"));
+
+        return User.builder().username(member.getUsername())
+                .password(member.getPassword())
+                .build();
+    }
 }
