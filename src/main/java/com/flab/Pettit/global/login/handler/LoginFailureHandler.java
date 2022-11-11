0b89ba503759d0c0cc5af6fcd73fb76bb0ae81c0
kidae92 <kidae92@hanmail.net>
2022-11-11 12:41:26 +0900

    로그인 실패 여부를 판단하는 handler 작성

--- src/main/java/com/flab/Pettit/global/login/handler/LoginFailureHandler.java
+++ src/main/java/com/flab/Pettit/global/login/handler/LoginFailureHandler.java
@@ -1,2 +1,22 @@
-package com.flab.Pettit.global.login.handler;public class LoginFailureHander {
+package com.flab.Pettit.global.login.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);//
+
+        response.getWriter().write("실패");
+        log.info("로그인에 실패했습니다");
+    }
 }
