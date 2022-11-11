00834a3dd5813c35b0430c2b75261a475d79c0f4
kidae92 <kidae92@hanmail.net>
2022-11-11 12:49:39 +0900

    인증 성공과 실패에 대한 기능 추가로 인한 SecurityConfig 수정

--- src/main/java/com/flab/Pettit/global/config/SecurityConfig.java
+++ src/main/java/com/flab/Pettit/global/config/SecurityConfig.java
@@ -1,8 +1,17 @@
-package com.flab.Pettit.config;
+package com.flab.Pettit.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.flab.Pettit.global.login.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.flab.Pettit.global.login.handler.LoginFailureHandler;
+import com.flab.Pettit.global.login.handler.LoginSuccessJWTProvideHandler;
+import com.flab.Pettit.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +30,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final LoginService loginService;
+    private final ObjectMapper objectMapper;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -37,8 +49,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() { // 1 - PasswordEncoder 등록
 
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() { // 2 - AuthenticationManager 등록
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessJWTProvideHandler loginSuccessJWTProvideHandler(){
+        return new LoginSuccessJWTProvideHandler();
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler(){
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter(){
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return jsonUsernamePasswordLoginFilter;
     }
 }
