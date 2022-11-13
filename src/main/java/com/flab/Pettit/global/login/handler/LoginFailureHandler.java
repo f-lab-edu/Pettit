e71447554f69c0f881b3c8fa8d461eeff7ad211e
kidae92 <kidae92@hanmail.net>
2022-11-13 13:57:05 +0900

    보안상 문제로 기존 response status인 SC_BAD_REQUEST에서 SC_OK로 수정

--- src/main/java/com/flab/Pettit/global/login/handler/LoginFailureHandler.java
+++ src/main/java/com/flab/Pettit/global/login/handler/LoginFailureHandler.java
@@ -8,13 +8,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+/**
+ * 로그인 실패처리를 담당하는 Handler
+ * 보안상 상태코드르 200으로 반환
+ * @author Gidae Hong
+ * @since 1.0
+ **/
 @Slf4j
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);//
+        response.setStatus(HttpServletResponse.SC_OK);//
 
         response.getWriter().write("실패");
         log.info("로그인에 실패했습니다");
